613f7df4c87bc8f65e0711f57862a8b9f798a9f3
GameTracking <83929805+GameTracking@users.noreply.github.com>
2021-05-15 12:35:56 -0300

    Atualizando arquivos
    Atualizando alguns arquivos...

--- GameTracking/js/app.js
+++ GameTracking/js/app.js
@@ -26,6 +26,19 @@ window.addEventListener("load",()=>{
             Logar();
         })
     }
+
+    //botao de busca
+    var btnBusca = document.querySelector('#btn-busca');
+
+    btnBusca.addEventListener('click',()=>{
+        buscar();
+    })
+
+    //barra de busca
+    var barraBusca = document.querySelector('#busca');
+    barraBusca.addEventListener('keyup',()=>{
+        buscar();
+    })
 });
 
 function EnviarSugestao(){
@@ -34,9 +47,22 @@ function EnviarSugestao(){
     var mensagem = document.querySelector("#mensagem").value;
 
     if ( nome == "" || email == "" || mensagem ==""){
-        alert("Preencha os campos necessários!");
+        Swal.fire(
+            'Erro',
+            'Preencha os campos necessários!',
+            'error'
+          )
     } else{
-        alert("Sugestão enviada!");
+        
+        Swal.fire(
+            'Sucesso',
+            'Sugestão "' + mensagem + '" enviada!',
+            'success'
+          )
+
+        setTimeout(()=>{
+            window.location.href = "index.html";
+        },2000)
     }
 }
 
@@ -48,39 +74,107 @@ function Cadastrar(){
     
     //Validação
 
-    if (nome == "" || email =="" || senha == "" || csenha == ""){
-        alert('Preencha os campos necessários!');
+    if (nome == "" || email == "" || senha == "" || csenha == ""){
+
+        Swal.fire(
+            'Erro',
+            'Preencha os campos necessários!',
+            'error'
+          )
+
     }else if(senha != csenha){
-        alert("As senhas não correspondem!")
+        Swal.fire(
+            'Erro',
+            'As senhas não correspondem!',
+            'error'
+          )
     }
     else{
-        alert('Cadastro realizado')
+        Swal.fire(
+            'Sucesso',
+            'Cadastro realizado!',
+            'success'
+          )
+
+        setTimeout(()=>{
+            window.location.href = "perfil.html";
+        },2000)
     }
 }
 
 function Logar(){
-    var nome = document.querySelector("#nome").value;
+    var email = document.querySelector("#email").value;
     var senha = document.querySelector("#senha").value;
     
     //Validação
 
-    if (nome == "" || senha == ""){
-        alert('Preencha os campos necessários!');
+    if (email == "" || senha == ""){
+        Swal.fire(
+            'Erro',
+            'Preencha os campos necessários!',
+            'error'
+          )
     }
     else{
-        alert('Logado com sucesso!');
-    }    
-}
+        Swal.fire(
+            'Sucesso',
+            'Logado com sucesso!',
+            'success'
+          )
+
+        setTimeout(()=>{
+            window.location.href = "perfil.html";
+        },2000)
+    }
+    }   
 
 
 function storage(){
     var nomeUsuario = document.querySelector('.nome-usuario');
     if(nomeUsuario){
-        localStorage.setItem("nome", "Jubileu");
+        localStorage.setItem("nome", "Rogério");
         nomeUsuario.innerHTML = localStorage.getItem("nome")
     }
     
 }
 
-
 window.onload = storage();
+
+
+//botao buscar
+
+function buscar(){
+
+
+    //valor do que o usuario digitou na barra de pesquisa
+    
+    var input = document.querySelector('#busca').value;
+    var filtro = input.toUpperCase();
+
+    //div onde esta informações dos jogos
+    var conteudo = document.querySelectorAll('.infos');
+
+    conteudo.forEach((x)=>{
+        var nomeJogo = x.childNodes[1].innerHTML;
+
+        var valor = nomeJogo.toUpperCase().indexOf(filtro);
+
+        if(nomeJogo.toUpperCase().indexOf(filtro)> -1){
+            x.style.display = "";
+            x.parentNode.style.display = "";
+            x.childNodes[1].style.display = "";
+
+        }
+        else{
+
+            x.style.display = "None";
+            x.parentNode.style.display = "None";
+            x.childNodes[1].style.display = "None";            
+        }
+    })
+}
+
+
+
+
+
